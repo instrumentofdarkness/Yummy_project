a7c222266ac7d570ce1de9745b31fdd9417c9b99
instrumentofdarkness <127889101+instrumentofdarkness@users.noreply.github.com>
2023-10-12 23:07:43 +0200

    recipe page update

--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -1,7 +1,40 @@
-import React, { useEffect, useState} from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const recipiesUrl = 'www.themealdb.com/api/json/v1/1/search.php?s=${userInput}';
 
 export default function RecipePage() {
+  const [recipe, setRecipe] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(recipiesUrl)
+      .then((response) => {
+        setRecipe(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <div>RecipePage</div>
-  )
+    <div>
+      <h1>Recipe Page</h1>
+      <div className="recipe-container">
+        {recipe.map((recipe) => (
+          <>
+            <div key={recipe.idMeal} className="recipe-card">
+              <h3>{recipe.strMeal}</h3>
+              <img src={recipe.strMealThumb} alt="recipe" />
+              <p>{recipe.strInstructions}</p>
+              <Link to="/favouritesPage">
+                <button>Add to Favourites</button>
+              </Link>
+            </div>
+          </>
+        ))}
+      </div>
+    </div>
+  );
 }
