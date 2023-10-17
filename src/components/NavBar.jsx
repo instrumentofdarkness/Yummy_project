ee6be2d7f76215d02fd61bf0e330c26708725bfd
instrumentofdarkness <127889101+instrumentofdarkness@users.noreply.github.com>
2023-10-17 16:52:46 +0200

    RecipiesList update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <div className="navBar">
-      <Link to="/HomePage">Home</Link>
+      <Link to="/homePage">Home</Link>
       <Link to="/recipes">Recipes</Link>
       <Link to="/favouritesPage">Favourites</Link>
       <Link to="/contactPage">Contact</Link>
--- src/components/RecipesList.jsx
+++ src/components/RecipesList.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import RecipesItem from "./RecipesItem";
 
 export default function RecipesList({ recipes, setFavRecipes, favRecipes }) {
-
-    return (
-      <div className="recipes">
-       
-
+  return (
+    <div>
+      {recipes.map((recipes) => {
+        return (
           <RecipesItem
- 
+            key={recipes.idMeal}
+            recipes={recipes}
+            setFavRecipes={setFavRecipes}
+            favRecipes={favRecipes}
           />
-      
-      </div>
-    );
-  }
-
+        );
+      })}
+    </div>
+  );
+}
